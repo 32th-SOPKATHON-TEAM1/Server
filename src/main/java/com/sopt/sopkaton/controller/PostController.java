76cf09e1ffd473af214756f7825a594c05ab6fbd
soohyun <sss4920@naver.com>
2023-05-20 22:34:42 +0900

    [feat] controller 구현

--- src/main/java/com/sopt/sopkaton/controller/PostController.java
+++ src/main/java/com/sopt/sopkaton/controller/PostController.java
@@ -1,7 +1,28 @@
 package com.sopt.sopkaton.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.sopt.sopkaton.common.ApiResponse;
+import com.sopt.sopkaton.controller.req.PostReqDto;
+import com.sopt.sopkaton.controller.res.PostResDto;
+import com.sopt.sopkaton.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/post")
 public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/{postId}")
+    public ApiResponse<PostResDto> getPost(@PathVariable Long postId){
+        return null;
+    }
+
+    @PostMapping
+    public ApiResponse<PostResDto> savePost(@RequestBody PostReqDto request){
+        return null;
+    }
+
 }
