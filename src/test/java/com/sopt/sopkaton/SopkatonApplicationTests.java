146ffbc630869f008e84fcd4988046528c11bdc9
KWY <wodrk1001@gmail.com>
2023-05-20 22:07:57 +0900

    init project

---
+++ src/test/java/com/sopt/sopkaton/SopkatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.sopkaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopkatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
