4e5d52443f3aa8d37c321257865d4b279d46a07a
KWY <wodrk1001@gmail.com>
2023-05-21 06:10:02 +0900

    [feat] web 연결

--- src/main/java/com/sopt/sopkaton/controller/PostController.java
+++ src/main/java/com/sopt/sopkaton/controller/PostController.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import retrofit2.Call;
 import retrofit2.Response;
 
+@CrossOrigin("https://emotion-studio.netlify.app")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/post")
